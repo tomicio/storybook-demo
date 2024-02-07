60f9e54bf366387be9fdfdef2b7f9bcfc1c15fb6
Tommy Lebel <tommy@tomicio.com>
2024-02-07 08:55:20 -0700

    feat: add vuetify

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,28 @@
+<script setup>
+import { ref, defineProps } from 'vue'
+
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  },
+})
+
+const value = ref(0.4)
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    
+    <v-slider
+      v-model="value"
+      :min="0"
+      :max="1"
+      :step="0.2"
+      thumb-label
+    ></v-slider>
+
   </div>
 </template>
 
